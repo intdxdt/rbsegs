997e776d07980fdcd39483f190c3d08edee77268
Titus Tienaah <titus.intdxdt@gmail.com>
2017-11-11 20:05:42 -0400

    pub

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,8 @@ extern crate float_eq;
 mod seg;
 mod brutelist;
 mod event;
-pub mod bruteforce;
 
-use bruteforce::brute_force;
+use seg::intersects;
 use brutelist::{new_brute_force_list, add_segment, BrutForceList};
 use event::{prepare_events, CREATE_RED, CREATE_BLUE, REMOVE_RED, REMOVE_BLUE};
 
@@ -24,7 +23,7 @@ pub fn rb_intersection(red: &[Vec<Vec<f64>>], blue: &[Vec<Vec<f64>>]) -> Vec<Vec
 
 
 pub fn red_blue_line_segment_intersection(red: &[Vec<Vec<f64>>], blue: &[Vec<Vec<f64>>],
-                                      visit: &mut FnMut(usize, usize) -> bool) -> bool {
+                                          visit: &mut FnMut(usize, usize) -> bool) -> bool {
     let nr = (red).len();
     let nb = (blue).len();
     let n = nr + nb;
@@ -59,6 +58,28 @@ pub fn red_blue_line_segment_intersection(red: &[Vec<Vec<f64>>], blue: &[Vec<Vec
     }
 
     ret
+}
+
+
+pub fn brute_force(red: &[Vec<Vec<f64>>], blue: &[Vec<Vec<f64>>]) -> Vec<Vec<usize>> {
+    let nr = red.len();
+    let nb = blue.len();
+    let mut crossings: Vec<Vec<usize>> = Vec::new();
+
+    for i in 0..nr {
+        let rseg = &red[i];
+        //let a    = rseg[0]
+        //let b    = rseg[1]
+        for j in 0..nb {
+            let bseg = &blue[j];
+            //c := bseg[0]
+            //d := bseg[1]
+            if intersects(&rseg[0], &rseg[1], &bseg[0], &bseg[1]) {
+                crossings.push(vec![i, j])
+            }
+        }
+    }
+    return crossings;
 }
 
 
